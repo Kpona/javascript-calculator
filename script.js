f101f3e179d6bd54bd5cba3d44544ee8b7320bbb
Kpona <105780570+Kpona@users.noreply.github.com>
2022-06-21 21:47:48 +0100

    javascript calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+﻿//Reading of numbers in the program
+const num1 = parseFloat(prompt('Enter Num1: '));
+//Readind of operators in the program
+const operator = prompt('Enter Operator to use: (+, -, *, /)');
+const num2 = parseFloat(prompt('Enter Num2: '))//convert string to number to perform
+
+let result = 0;
+if (isNaN(num1) || isNaN(num2)) {
+  alert('Wrong input! Refresh and try again.');
+} else {
+  if (operator == '+') {
+    result = num1 + num2;
+  } else if (operator == '-') {
+    result = num1 - num2;
+  } else if (operator == '*') {
+    result = num1 * num2;
+  } else if (operator == '/') {
+    result = num1 / num2;
+  }
+  alert(num1 + ' ' + operator + ' ' + num2 + ' = ' + result)
+}
